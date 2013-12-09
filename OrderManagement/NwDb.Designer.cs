d3ff0081a6c423b1b5f914cc84698b67e1d45739
ddana0909 <dana.diac@yahoo.com>
2013-12-09 01:22:22 +0000

    del

--- OrderManagement/NwDb.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model 'D:\DANA\FACULTATE\anul III\sem I\RichApplicationDevelopment\OrderManagement\OrderManagement\NwDb.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
