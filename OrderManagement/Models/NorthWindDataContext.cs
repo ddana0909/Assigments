162d1b2c24c8c354ff00c2984e61ac626a7fbe29
ddana0909 <dana.diac@yahoo.com>
2013-12-09 01:52:16 +0000

    final commit

---
+++ OrderManagement/Models/NorthWindDataContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+
+
+namespace OrderManagement
+{
+    public class NorthWindDataContext : DbContext
+    {
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CustomerDemographic> CustomerDemographics { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Order_Detail> Order_Details { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Shipper> Shippers { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Territory> Territories { get; set; }
+      
+        public DbSet<Invoice> Invoices { get; set; }
+       
+    }
+}
