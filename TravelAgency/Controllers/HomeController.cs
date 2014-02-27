761e3b97ee2a71b695a4744f01dd48f387556ff6
ddana0909 <dana.diac@yahoo.com>
2014-02-27 22:11:45 +0000

    add timeline on trip with progressbar

--- TravelAgency/Controllers/HomeController.cs
+++ TravelAgency/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using TravelAgency.DAL;
+using TravelAgency.HelperClasses;
 using TravelAgency.Models;
 
 
@@ -8,7 +11,7 @@ namespace TravelAgency.Controllers
 {
     public class HomeController : Controller
     {
-        
+
 
         private readonly ITravelAgencyRepository _repository;
 
@@ -16,7 +19,7 @@ namespace TravelAgency.Controllers
         {
             _repository = repository;
         }
-       
+
 
         public ActionResult Index()
         {
@@ -62,7 +65,72 @@ namespace TravelAgency.Controllers
             ViewBag.Start = leg.StartLocation;
             ViewBag.Finish = leg.FinishLocation;
             ViewBag.LegId = leg.Id;
-            return PartialView("_GuestList",guests);
+            return PartialView("_GuestList", guests);
+        }
+
+        public ActionResult GetProgressBar(int tripId)
+        {
+
+            var trip = _repository.GetTrip(tripId);
+            var legs = _repository.GetLegsForTrip(tripId);
+            string[] cssClasses = new string[3] { "progress-bar progress-bar-success", 
+                                                  "progress-bar progress-bar-info", 
+                                                  "progress-bar progress-bar-warning"}
+                                                  ;
+
+
+            var tripPeriod = trip.FinishDate.Subtract(trip.StartDate).TotalDays;
+            var list = new List<TripTimelineElement>();
+            int i = 0;
+            var auxFinish = trip.StartDate;
+            foreach (var leg in legs)
+            {
+
+                //inseamna ca avem gap
+                if (auxFinish != leg.StartDate)
+                {
+                    var period = leg.StartDate.Subtract(auxFinish).TotalDays;
+                    var yyy = new TripTimelineElement
+                    {
+                        StartDate = auxFinish.ToShortDateString(),
+                        FinishDate = leg.StartDate.ToShortDateString(),
+                        Percentage = (int)(period / tripPeriod * 100),
+                        CssClass = "progress-bar progress-bar-danger",
+                        NotPlanned = true
+                    };
+
+                    list.Add(yyy);
+                }
+
+                {
+                    var legPeriod = leg.FinishDate.Subtract(leg.StartDate).TotalDays;
+                    var yyy = new TripTimelineElement
+                    {
+                        Percentage = (int)(legPeriod / tripPeriod * 100),
+                        StartDate = leg.StartDate.ToShortDateString(),
+                        FinishDate = leg.FinishDate.ToShortDateString(),
+                        CssClass = "progress-bar progress-bar-success",
+                        NotPlanned = false
+
+                    };
+                    list.Add(yyy);
+                }
+              
+                auxFinish = leg.FinishDate;
+            }
+            if(auxFinish!=trip.FinishDate)
+                list.Add(
+            new TripTimelineElement{
+                Percentage=(int)(trip.FinishDate.Subtract(auxFinish).TotalDays/tripPeriod*100),
+                StartDate = auxFinish.ToShortDateString(),
+                FinishDate = trip.FinishDate.ToShortDateString(),
+                CssClass = "progress-bar progress-bar-danger",
+                NotPlanned = true
+            }
+        );
+            
+
+            return View("_TimeLine", list);
         }
     }
 }
